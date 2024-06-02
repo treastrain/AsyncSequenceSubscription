18a02aae1e8e7219f1a583f778866fe1cfec56f1
treastrain / Tanaka Ryoga <tr@tret.jp>
2024-06-02 23:27:55 +0900

    Create TaskCancellerStoreTests.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,9 @@ let package = Package(
     targets: [
         .target(
             name: "TaskCancellerStore"),
+        .testTarget(
+            name: "TaskCancellerStoreTests",
+            dependencies: ["TaskCancellerStore"]),
     ]
 )
 
